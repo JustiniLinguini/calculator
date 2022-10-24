a969d9dcd0d8e58860f1a8ac220606fe72283a37
JustiniLinguini <justinusprime@gmail.com>
2022-10-24 13:55:01 -0400

    Refactor scripting: replace stack functionality
    Replaced the stack system with a more streamlined system that stores
    operator, operand, and current display in their own variables.
    This made adding new features and fixing bugs much more easy.

--- script.js
+++ script.js
@@ -1,105 +1,91 @@
+let display = "";
+let operator = "";
+let operand = "";
+
 
 function setHandlers(){
     const buttonSelector = document.querySelectorAll('.button-selector');
     const screen = document.querySelector('#screen');
     const equals = document.querySelector('#equals');
-    let equalsPressed = false;
+    const backspace = document.querySelector('#backspace');
+
     buttonSelector.forEach((button) => {
-        button.addEventListener('click' , ()=> {
-            screen.textContent = screen.textContent + `${button.id}`;
-            buttonString = buttonString + `${button.id}`;
-            let sum = stackHandler(buttonString);
-            console.log(buttonString, "input");
-
-            if(sum != undefined){
-                screen.textContent = sum + lastChar;
-            }
-
+        button.addEventListener('click' , () => {
+            calc(button.id);
+            screen.textContent = `operand: ${operand} operator:${operator} display: ${display}`;
         })
     });
-    equals.addEventListener('click',() =>{
-        let equalsSum = stackHandler(buttonString, true);
-        console.log(buttonString, "string e");
 
-        if(equalsSum != undefined){
-            screen.textContent = equalsSum;
+    equals.addEventListener('click', () => { 
+
+        if(operator != "" && operand != "" && display != ""){
+            let result = eval(operand, operator, display)
+            operand = result;
+            operator = "";
+            display = "";
+            lastChar = "";
+            screen.textContent = `operand: ${operand} operator:${operator} display: ${display}`;
+        } else {
+            screen.textContent = `operand: ${operand} operator:${operator} display: ${display}`;
         }
+    });
+
+    backspace.addEventListener('click', () => {
+        display = display.slice(0, -1);
+        screen.textContent = `operand: ${operand} operator:${operator} display: ${display}`;
+        lastChar = display.charAt(display.length-1);
     })
-
 }
 
-let buttonString = "";
-let stack = [];
 
-function stackHandler(str, equalsPressed = false){
-    lastChar = str.charAt(str.length -1);
-    let hasOperator = false;
-    console.log(stack, 'stack');
+isOperator = char => char == "+" || char == "-" || char == "*" || char == "/";
+
+calc = (buttonId) => { 
+    if(buttonId != 'backspace'){
+        display += buttonId;
+    }
+    lastChar = display.charAt(display.length-1);
+
+    if(isOperator(lastChar) == true && operator != ""){
+        let result = eval(operand, operator, display)
+        operand = result;
+        operator = lastChar;
+        display = "";
+        lastChar = "";
+    }
+
     if(isOperator(lastChar) == true){
-        hasOperator = true;
-        if(str.length > 1){
-            stack.push(str.slice(0,-1 ));
+        if(operand == ""){
+            operand = display.slice(0, -1);
         }
-        stack.push(lastChar);
-        buttonString = "";
+        operator = lastChar;
+        display = "";
+        lastChar = "";
+        
     }
-    if(stack.length > 3){
-        stack.pop();
-        sum = sumStack(stack);
-        stack = [];
-        stack.push(sum);
-        stack.push(lastChar);
-        console.log('STACK = ' , stack)
-        buttonString = "";
-        return sum;
-    }
-
-    if(equalsPressed == true){
-        console.log("i got pressed");
-        stack.push(str);
-        sum = sumStack(stack)
-        stack = [];
-        stack.shift();
-        stack.push(sum)
-        console.log(stack, "e")
-        buttonString = "";
-        equalsPressed = false;
-        return sum;
-    }
+ 
 }
 
-//takes an array with 3 items, items should include one operator and 2 operands
-function sumStack(equation){
+eval = (operand, operator, display) => {
 
-    if(equation.length == 3){
-        let operand1 = parseInt(equation[0]);
-        let operand2 = parseInt(equation[2]);
-        let operator = equation[1];
-
-        if(operator == "+"){
-            return operand1 + operand2;
-        }
-        if(operator == "-"){
-            return operand1 - operand2;
-        }
-        if(operator == "*"){
-            return operand1 * operand2;
-        }
-        if(operator == "/"){
-            return operand1 / operand2;
-        }
-        else{
-            return "invalid operator";
-        }
+    if(operator == "+"){
+        return parseInt(operand) + parseInt(display);
     }
+
+    if(operator == "-"){
+        return parseInt(operand) - parseInt(display);
+    }
+
+    if(operator == "*"){
+        return parseInt(operand) * parseInt(display);
+    }
+
+    if(operator == "/"){
+        return parseInt(operand) / parseInt(display);
+    } else {
+        return "INVALID OPERATOR";
+    }
+
 }
 
-function isOperator(char){
-
-    return char == "+" || char == "-" || char == "*" || char == "/";
-}
-
-function inputHandler(str){
-    
-}
 setHandlers();
